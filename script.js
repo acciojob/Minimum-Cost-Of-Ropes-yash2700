6c4e3b527778688831453aec715887cfe50a6ebb
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-03-16 20:43:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function calculateMinCost() {
   //your code here
-  
+  var res=0;
+    while(a.length!=1){
+        a=a.sort((b,c)=>{return b-c});
+        c=a.shift()
+        d=a.shift()
+        sum=c+d;
+        a.unshift(sum)
+        res+=sum
+    }
+    return res;
   
   
 }  
